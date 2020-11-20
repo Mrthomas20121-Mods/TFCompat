c035274d03029083aaba54f49322d5704e80a579
Thomas <thomas77300@gmail.com>
2020-11-20 12:41:07 +0100

    Bumped version to 1.2.0

--- src/main/java/mrthomas20121/tfcompat/TFCompat.java
+++ src/main/java/mrthomas20121/tfcompat/TFCompat.java
@@ -1,11 +1,8 @@
 package mrthomas20121.tfcompat;
 
 import mrthomas20121.tfcompat.client.GuiHandler;
-import mrthomas20121.tfcompat.compat.rustic.RusticModule;
 import mrthomas20121.tfcompat.library.ModuleCore;
 import mrthomas20121.tfcompat.library.ModuleManager;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -19,7 +16,7 @@ public class TFCompat
 {
     public static final String MODID = "tfcompat";
     public static final String NAME = "TFCompat";
-    public static final String VERSION = "1.1.4";
+    public static final String VERSION = "1.2.0";
     public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2847,);"
             + "required-after:rocksalt@[1.0.1,);"
             + "required-after:tfc;"
@@ -29,6 +26,7 @@ public class TFCompat
             + "after:pyrotech;"
             + "after:mekanism;"
             + "after:improvedbackpacks;"
+            + "after:techreborn;"
             + "after:thermalexpansion;"
             + "after:thaumcraft;";
 
@@ -52,10 +50,6 @@ public class TFCompat
         for(ModuleCore module : ModuleManager.getModules())
         {
             module.preInit(event);
-        }
-        if(Loader.isModLoaded("rustic"))
-        {
-            MinecraftForge.EVENT_BUS.register(RusticModule.class);
         }
     }
 
